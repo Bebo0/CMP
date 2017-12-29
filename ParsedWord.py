72f222c422dc93a6d638702b7be8d114e55c6da2
Bebo Elhosary <beboelhosary@hotmail.com>
2017-12-29 15:21:50 -0800

    Deleted Word and ParsedWord modules as they are not needed.

--- ParsedWord.py
+++
@@ -1,6 +0,0 @@
-class ParsedWord:
-
-	reddit = authenticate() #authenticate called here so that only 1 authentication occurs even if multiple objects are instantiated
-
-	# CONSTRUCTOR:
-	def __init__(self, subredditname):
