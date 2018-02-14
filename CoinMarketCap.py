4b323e4b3ceca1d12bd8464fb182fc9e2a0d8bf5
Bebo Elhosary <beboelhosary@hotmail.com>
2018-02-14 13:22:52 -0800

    getCoins() now writes all coins' names/symbols to Coins.txt

--- CoinMarketCap.py
+++ CoinMarketCap.py
@@ -4,20 +4,35 @@ import string
 from collections import Counter
 
 def getCoins():
-	testArr = []
+	# testArr = []
+
+	file  = open("Coins.txt","w")
+	# file2 = open("Rankings2.txt","w")
+
+	# file .write(repr(self.ranking) + '\n' )
+	# file2.write(repr(self.ranking2) + '\n' )
+
+	
+	# file2.close()
 	
 	r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=2000')
 	for coin in r.json():
-		test = Counter()
+		# test = Counter()
 		strong = ''.join(coin["name"]).lower().encode('ascii','ignore')
 		strong1 = ''.join(coin["symbol"]).lower().encode('ascii','ignore')
-		test[strong] = strong1
 
-		testArr.append(test)
+		file .write(repr(strong) )
+		file .write(repr(strong1) + '\n' )
+
+		# test[strong] = strong1
+
+		# testArr.append(test)
 	    #print(coin["name"].lower())
 	    #print(coin["symbol"].lower())
 	    #print("\n")
-	print(testArr)
+	    #
+	file .close()
+	# print(testArr)
 
 
 def main():
