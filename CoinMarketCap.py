f109010cb594312a905994c4d2e9b82b6d65cd42
Bebo Elhosary <beboelhosary@hotmail.com>
2018-02-14 13:16:08 -0800

    Added getCoins(). Adds all coins to a list

--- CoinMarketCap.py
+++ CoinMarketCap.py
@@ -1,9 +1,48 @@
 import json 
 import requests 
 import string
+from collections import Counter
 
-r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=2000')
-for coin in r.json():
-    print(coin["name"].lower())
-    print(coin["symbol"].lower())
-    #print("\n")
+def getCoins():
+	testArr = []
+	
+	r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=2000')
+	for coin in r.json():
+		test = Counter()
+		strong = ''.join(coin["name"]).lower().encode('ascii','ignore')
+		strong1 = ''.join(coin["symbol"]).lower().encode('ascii','ignore')
+		test[strong] = strong1
+
+		testArr.append(test)
+	    #print(coin["name"].lower())
+	    #print(coin["symbol"].lower())
+	    #print("\n")
+	print(testArr)
+
+
+def main():
+
+	
+	#test["bitcoin"] = "btc"
+	#print(test)
+	getCoins()
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+def getCoins():
+	testArr = []
+	
+	r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=2000')
+	for coin in r.json():
+		test = Counter()
+		test[coin["name"].lower()] = coin["symbol"].lower()
+
+		testArr.append(test)
+	    #print(coin["name"].lower())
+	    #print(coin["symbol"].lower())
+	    #print("\n")
+	print(testArr)
