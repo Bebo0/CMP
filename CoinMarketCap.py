7adc8d63aace2da92406d8b12307a94a150d410a
Bebo Elhosary <beboelhosary@hotmail.com>
2018-06-05 19:04:39 -0700

    Program now Python 3 compatible. Print sentiment scores

--- CoinMarketCap.py
+++ CoinMarketCap.py
@@ -14,7 +14,7 @@ def getCoins():
 	r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
 	for coin in r.json():
 		strong = str(coin)
-		strong = coin["name"].lower().encode('ascii','ignore')
+		strong = coin["name"].lower() # encode('ascii','ignore')
 
 		# removes spaces in a coin's name
 		strong = re.sub('[\s+]', '', strong)
